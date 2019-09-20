d7ca4d4617cc67201142b9cca9018c34e44e3006
Krupin <krupini-adm@office.lawpro.ca>
2019-09-20 17:02:38 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 import Amplify, { Auth } from 'aws-amplify';
+import Login from './login/Login.jsx';
 
 Amplify.configure({
   Auth: {
@@ -35,21 +36,8 @@ function App() {
   });
   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Login />
     </div>
   );
 }
---
+++ src/login/Login.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+function Login(props) {
+    return <div><span>hello</span></div>;
+}
+
+export default Login;
